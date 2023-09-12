b614ebb4a1d8a5ae2ce432a9a43423a3a0f3c3c0
tolgabayrakdev <bayraktolga28@gmail.com>
2023-09-12 09:56:52 +0300

    notfound pafe added for root project layput

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -26,24 +26,7 @@ const NotFoundPage = () => {
                                 <span className="text-600 lg:text-lg">Ultricies mi quis hendrerit dolor.</span>
                             </span>
                         </Link>
-                        <Link href="/" className="w-full flex align-items-center py-5 border-300 border-bottom-1">
-                            <span className="flex justify-content-center align-items-center bg-orange-400 border-round" style={{ height: '3.5rem', width: '3.5rem' }}>
-                                <i className="pi pi-fw pi-question-circle text-50 text-2xl"></i>
-                            </span>
-                            <span className="ml-4 flex flex-column">
-                                <span className="text-900 lg:text-xl font-medium mb-1">Solution Center</span>
-                                <span className="text-600 lg:text-lg">Phasellus faucibus scelerisque eleifend.</span>
-                            </span>
-                        </Link>
-                        <Link href="/" className="w-full flex align-items-center mb-5 py-5 border-300 border-bottom-1">
-                            <span className="flex justify-content-center align-items-center bg-indigo-400 border-round" style={{ height: '3.5rem', width: '3.5rem' }}>
-                                <i className="pi pi-fw pi-unlock text-50 text-2xl"></i>
-                            </span>
-                            <span className="ml-4 flex flex-column">
-                                <span className="text-900 lg:text-xl font-medium mb-1">Permission Manager</span>
-                                <span className="text-600 lg:text-lg">Accumsan in nisl nisi scelerisque</span>
-                            </span>
-                        </Link>
+
                     </div>
                 </div>
             </div>
